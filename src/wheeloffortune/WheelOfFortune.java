abbecf6ca3e105a1e18dfefc887f7f1acda5fc9b
eot5104 <eot5104@OZSTHLND307-100.win.psu.edu>
2016-09-16 11:44:47 -0400

    allowed input letters

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -7,14 +7,36 @@
 * ID: eot5104
 */
 package wheeloffortune;
+import java.util.Scanner;
 
 public class WheelOfFortune {
+    
 
   /**
    * @param args the command line arguments
    */
   public static void main(String[] args) {
+      String letter;
+      
+      Scanner keyboard = new Scanner(System.in);
+      System.out.println("Spin the wheel");
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Guess a letter");
+      char c = scanner.next().charAt(0);
+    System.out.println("You have entered: "+c);
+    
+    char x = 26;
+    
+    
+    
+    
+      
+  }
+     
+      
+      
+     
     
   }
   
-}
+
