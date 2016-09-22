6dea940b4b2991d50ff3459e17a3339398a2c10e
PSU <PSU@130.203.105.194>
2016-09-22 14:24:54 -0400

    added by elaine

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -16,20 +16,21 @@ public class WheelOfFortune {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-      String letter;
       
       Scanner keyboard = new Scanner(System.in);
-      System.out.println("Spin the wheel");
       Scanner scanner = new Scanner(System.in);
-      System.out.println("Guess a letter");
       char c = scanner.next().charAt(0);
-    System.out.println("You have entered: "+c);
-    
-    char x = 26;
-    
-    
-    
-    
+      options();
+        
+      
+  }
+  public static void options(){
+      String asdf;
+      Scanner options = new Scanner(System.in);
+      System.out.println("  ====================\n  "+
+            "     =Wheel of Fortune  =\n  "
+            +"   =====================\n  ");
+      asdf = options.next();
       
   }
      
