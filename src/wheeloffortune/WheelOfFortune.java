3a2bc8fe9fe20bf95b09a4cbe28b0affcdadada1
eot5104 <huskymango@192.168.1.124>
2016-09-25 04:43:34 -0400

    done

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -8,65 +8,119 @@
 package wheeloffortune;
     
 
-import java.util.List; 
+
 import java.util.Scanner;
 import java.util.Random;
+
 public class WheelOfFortune {
-    public static void main(String[] args){
-       input();
-      
-      
-    }
     
-    public static void input(){
+
+    public static void main(String[] args){
+        String puzzle = "The quick brown fox jumps over the lazy dog"; 
+        String letter;            
+        Scanner guess; 
         
-        String puzzle = "The quick brown fox jumps over the lazy dog";
-        String letter;
-        String choice;
-        int input;
-        Scanner keyboard = new Scanner(System.in);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\n================================\n"+
+        guess =  new Scanner(System.in);
+        
+        
+        Scanner scanner = new Scanner(System.in); //getting input from the same keyboard
+        System.out.println("\n================================\n"+//Title
                 "       =WHEEL OF FORTUNE=\n"
                 + "================================\n"
-                +"1. Spin the wheel\n"
-                +"2. Buy a vowel\n"
-                +"3. Solve the puzzle\n"
-                +"4. Quit the game\n"
-                +"5. Test Letter Input\n"
-                +"Choice: \n");
+                +"1. Spin the wheel\n"//Spin the wheel
+                +"2. Buy a vowel\n"//buy a vowel
+                +"3. Solve the puzzle\n"//solve the puzzle
+                +"4. Quit the game\n"//quit the game
+                +"5. Toggle\n" //
+                +"9. Test Letter Input\n");
         
-        choice = scanner.next();
-        
-         while(true)
-      {
-      }
-          Scanner key = new Scanner(System.in);
-          input = keyboard.nextInt();
-          letter = key.nextLine();
-          while(true)
-      {
-          input = keyboard.nextInt();
-          if (input == 4){
-             break;
-          }
+         boolean done = false; //quitting the game
+         while(!done){
+         String input = guess.nextLine();
+         letter = scanner.next();
+         if (letter.matches("[A-z]")){
+                 System.out.println("You choose the letter:"+ letter);
+                }
+                else{
+                 System.out.println("This is not a letter");
+                   
+                    }
+         int choice = Integer.parseInt(input);//return integer of the string choice
+
             
           
-            switch(input)
-            {
-              case 1: System.out.println("You choose to spin the wheel."+letter);
-                      break;
-              case 2: System.out.println("You choose to buy a vowel."+letter);
-                      break;
-              case 3: System.out.println(" You choose to solve the puzzle."+letter);
-                      break;
-              case 4: System.out.println(" You choose to quit the game."+letter);
-                      System.exit(0);
-                      break;    
-              case 5: System.out.println("Test Input;");
-                        break;
+        switch(choice)
+          {
+             case 1:System.out.println("You choose to spin the wheel."+letter);//user chose to spin the wheel
+                     SpinWheel();
+                     Puzzle();
+                  
+                     System.out.println("You chose:");//prints out what the user chose
+              
+                      break;//stops the loop and moves on to the next statement
+              case 2:System.out.println("You choose to buy a vowel."+letter);//user chose to buy a vowel
+                     break;//stops the loop and moves on to the next statement
+              case 3:System.out.println(" You choose to solve the puzzle."+letter);//user chose to solve the puzzle
+                     break;//stops the loop and moves on to the next statement
+              case 4:System.out.println(" You choose to quit the game."+letter);//user chose to quit game
+                     System.exit(0);
+                     break;   //stops the loop and moves on to the next statement 
+              case 5:System.out.println(puzzle);
+                     int number;
+                     Scanner scan = new Scanner(System.in);
+                     System.out.println("Press 5 to untoggle the puzzle");
+                     number = scan.nextInt();
+              
+              break;//stops the loop and moves on to the next statement
+              case 9:System.out.println("Test Input;");
+                     break;//stops the loop and moves on to the next statement
             }
+         }
+    }
+    
+    public static void SpinWheel(){
+     
+        String[] WheelVal = {"$300","$300","$300","$300","$300","$350","$400",
+          "$400","$450","$500","$500","$500","$550","$600","$600","$600","$700","$800"//Random values of the wheel
+              , "$800","$900","$900","$5000","BANKRUPT", "LOSE A TURN"};
+        
+        Random random = new Random();//allow users to land on a random value
+        int index = random.nextInt(WheelVal.length); //pick random number from array
+        System.out.println("You landed on: "+WheelVal[index]);//shows what the user landed on
+    }
+    public static void Puzzle(){
+      String puzzle = "The quick brown fox jumps over the lazy dog"; //the answer to the puzzle
+      String input = " "; //string input blank
+      Scanner guess = new Scanner(System.in);
+      boolean puzzleNotSolved = true; 
+      while (puzzleNotSolved){ //convert string to character arrays
+          for (char unvieldLetter: puzzle.toCharArray()) {
+
+              if (input.indexOf(unvieldLetter) == -1) {
+                  System.out.print("_ "); //doesn't show the answers, covered with blanks
+                  puzzleNotSolved = true;
+                  
+              }
+              else{
+                  System.out.print(unvieldLetter);//print out blanks if the letter entered isn't true
+              }
+          }
+          if (! puzzleNotSolved ) {
+              break;
+              
       }
+           System.out.println("\nEnter a Letter: ");
+           String character = guess.next();
+           input += character;
+      }
+           
+           System.exit(0);//quits the game
+      }
+    }
+    
+        
+         
+      
           
             
             
